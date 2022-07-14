2c2f2e023085e57bd2804d31ff585da1cecf96eb
Daniel Kwon <skansmfqh7312@gmail.com>
2022-07-14 18:12:16 +0900

    label에 Dateformatter 적용 중

--- AnnivarsaryCalculator/AnnivarsaryCalculatorViewController.swift
+++ AnnivarsaryCalculator/AnnivarsaryCalculatorViewController.swift
@@ -16,10 +16,17 @@ class AnnivarsaryCalculatorViewController: UIViewController {
     @IBOutlet var dLabel: [UILabel]!
     @IBOutlet var annivarsaryYear: [UILabel]!
     
+    @IBOutlet weak var dPlus100Label: UILabel!
+    @IBOutlet weak var dPlus200Label: UILabel!
+    @IBOutlet weak var dPlus300Label: UILabel!
+    @IBOutlet weak var dPlus400Label: UILabel!
+    
     @IBOutlet weak var d100Label: UILabel!
     @IBOutlet weak var d200Label: UILabel!
     @IBOutlet weak var d300Label: UILabel!
     @IBOutlet weak var d400Label: UILabel!
+    
+    @IBOutlet weak var dDayDatePicker: UIDatePicker!
     
     
     override func viewDidLoad() {
@@ -61,7 +68,42 @@ class AnnivarsaryCalculatorViewController: UIViewController {
         
     }
     
+    let calendar = Calendar.current
+    let currentDate = Date()
+    var daysCount: Int = 0
+    let dateFormatter = DateFormatter()
+    
+//    func calculteDays() {
+//        dateFormatter.dateFormat = "yyyy-MM-dd"
+//        let startDate = DateFormatter.dateFormat(from:"")
+//        daysCount = days(from: startDate)
+//        let hundred = calendar.date(byAdding: .day, value: 100, to: startDate)
+//    }
+//
+//    func days(from date: Date) -> Int {
+//        return calendar.dateComponents([.day], from: date, to: currentDate).day + 1
+//    }
 
+    @IBAction func dDayDatePickerAction(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyy년-MM월-dd일"
+        d100Label.text = formatter.string(from: datePickerView.date)
+        d200Label.text = formatter.string(from: datePickerView.date)
+        d300Label.text = formatter.string(from: datePickerView.date)
+        d400Label.text = formatter.string(from: datePickerView.date)
+        
+        formatter.dateFormat = "+ dd"
+        dPlus100Label.text = formatter.string(from: datePickerView.date)
+        dPlus200Label.text = formatter.string(from: datePickerView.date)
+        dPlus300Label.text = formatter.string(from: datePickerView.date)
+        dPlus400Label.text = formatter.string(from: datePickerView.date)
+        
+    }
+    
+   
+    
     
 
 }
--- AnnivarsaryCalculator/ViewController.swift
+++ AnnivarsaryCalculator/ViewController.swift
@@ -11,7 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //DateFormatter: 알아보기 쉬운 날짜 + 시간대 (yyyy MM dd hh:mm:ss) mm은 분을 의미
+        let format = DateFormatter()
+        format.dateFormat = "+ dd"
+        //format.locale
+        
+        let result = format.string(from: Date())
+        print(result)
+        
+        let word = "2일"
+        let dateResult = format.date(from: word)
+        print(dateResult)
+        
     }
 
 
